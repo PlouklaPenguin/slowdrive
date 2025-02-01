28fe6c83c786ca32084ce45c61e117e13a448757
Sam Robinson <samjrobinson006@gmail.com>
2025-02-01 14:19:28 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,169 @@
+use std::f32::consts::FRAC_PI_2;
+
+mod perlin;
+
+use bevy::{
+    asset::RenderAssetUsages, color::palettes::basic::SILVER, input::mouse::AccumulatedMouseMotion, math::vec3, prelude::*, render::mesh::PrimitiveTopology, window::{CursorGrabMode, PrimaryWindow}
+};
+
+use std::hash::{DefaultHasher, Hash, Hasher};
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins,))
+        .add_systems(Startup, (setup))
+        .add_systems(Update, (move_camera, mouse_lock))
+        .run();
+}
+
+#[derive(Component)]
+struct World {
+    seed: f32,
+}
+
+#[derive(Component)]
+struct PlayerCamera {
+    speed: f32,
+}
+
+#[derive(Component, Debug, Deref, DerefMut)]
+struct CameraSensitivity(Vec2);
+
+impl Default for CameraSensitivity {
+    fn default() -> Self {
+        Self(Vec2::new(0.003, 0.002))
+    }
+}
+
+fn spawn_world(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList, RenderAssetUsages::default());
+
+
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    
+
+    
+    commands.spawn((
+        Camera3d::default(),
+        Transform::from_xyz(10.0, 12.0, 16.0).looking_at(Vec3::ZERO, Vec3::Y),
+        PlayerCamera { speed: 10.0 },
+        CameraSensitivity::default(),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Cuboid::new(1., 1., 1.))),
+        MeshMaterial3d(materials.add(Color::Srgba(SILVER))),
+        Transform::from_xyz(0.0, 0.5, 0.0),
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Circle::new(4.0))),
+        MeshMaterial3d(materials.add(Color::BLACK)),
+        Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+    ));
+
+    commands.spawn((
+        PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        Transform::from_xyz(4.0, 8.0, 4.0),
+    ));
+}
+
+fn move_camera(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    timer: Res<Time>,
+    mouse_motion: Res<AccumulatedMouseMotion>,
+    mut query: Query<(&mut Transform, &PlayerCamera, &CameraSensitivity)>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let (mut c_transform, camera, camera_sensitivity) = query.single_mut();
+    let immut_trans = c_transform.clone();
+
+    let p_window = window_query.single();
+
+    if keyboard_input.pressed(KeyCode::Space) {
+        c_transform.translation += Vec3::Y * camera.speed * timer.delta_secs();
+    }
+
+    if keyboard_input.pressed(KeyCode::ControlLeft) {
+        c_transform.translation -= Vec3::Y * camera.speed * timer.delta_secs();
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyW) {
+        c_transform.translation -= vec3(immut_trans.local_z().x, 0., immut_trans.local_z().z)
+            * camera.speed
+            * timer.delta_secs();
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyS) {
+        c_transform.translation += vec3(immut_trans.local_z().x, 0., immut_trans.local_z().z)
+            * camera.speed
+            * timer.delta_secs();
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyA) {
+        c_transform.translation -= vec3(immut_trans.local_x().x, 0., immut_trans.local_x().z)
+            * camera.speed
+            * timer.delta_secs();
+    }
+
+    if keyboard_input.pressed(KeyCode::KeyD) {
+        c_transform.translation += vec3(immut_trans.local_x().x, 0., immut_trans.local_x().z)
+            * camera.speed
+            * timer.delta_secs();
+    }
+
+    let delta = mouse_motion.delta;
+
+    if delta != Vec2::ZERO {
+        let delta_yaw = -delta.x * camera_sensitivity.x;
+        let delta_pitch = -delta.y * camera_sensitivity.y;
+
+        let (yaw, pitch, roll) = c_transform.rotation.to_euler(EulerRot::YXZ);
+        let yaw = yaw + delta_yaw;
+
+        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
+        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
+
+        c_transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+    }
+}
+
+fn mouse_lock(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut q_windows: Query<&mut Window, With<PrimaryWindow>>,
+) {
+    let mut primary_window = q_windows.single_mut();
+
+    if keyboard_input.just_pressed(KeyCode::Escape) {
+        if primary_window.cursor_options.grab_mode == CursorGrabMode::Locked {
+            primary_window.cursor_options.grab_mode = CursorGrabMode::None;
+            primary_window.cursor_options.visible = true;
+        } else {
+            primary_window.cursor_options.grab_mode = CursorGrabMode::Locked;
+            primary_window.cursor_options.visible = false;
+        }
+    }
+}
+
+// fn perlin(x: u32, y: u32) -> u32 {
+//     let hash = calculate_hash(&(x, y));
+// }
+
+// fn calculate_hash<T: Hash>(t: &T) -> u64 {
+//     let mut s = DefaultHasher::new();
+//     t.hash(&mut s);
+//     s.finish()
+// }
---
+++ src/perlin.rs
@@ -0,0 +1,130 @@
+use bevy::math::{
+    vec2,
+    Vec2,
+};
+use core::ops::{Add, Mul, Sub};
+use rand::{
+    distributions::{Distribution, Standard},
+    seq::SliceRandom,
+    Rng, SeedableRng,
+};
+use rand_xorshift::XorShiftRng;
+
+const TABLE_SIZE: usize = 256;
+
+trait Quintic {
+    fn map_quintic(&self) -> Self;
+}
+
+impl Quintic for f32 {
+    fn map_quintic(&self) -> Self {
+        let x = self.clamp(0.0, 1.0);
+
+        x * x * x * (x * (x * 6.0 - 15.0) + 10.0)
+    }
+}
+
+trait NoiseHasher: Send + Sync {
+    fn hash(&self, to_hash: &[isize]) -> usize;
+}
+
+#[inline(always)]
+pub fn linear<T>(a: T, b: T, alpha: f32) -> T
+where
+    T: Mul<f32, Output = T> + Add<Output = T>,
+{
+    b * alpha + a * (1.0 - alpha)
+}
+
+
+#[inline(always)]
+pub fn perlin_2d<NH>(point: Vec2, hasher: &NH) -> f32
+where
+    NH: NoiseHasher + ?Sized,
+{
+    const SCALE_FACTOR: f32 = 2.0 / std::f32::consts::SQRT_2;
+
+    let corner = point.floor();
+    let distance = point - corner;
+
+    macro_rules! call_gradient(
+        ($x:expr, $y:expr) => {
+            {
+            let offset = vec2($x, $y);
+            let point = distance - offset;
+
+            match hasher.hash({
+                &[(corner + offset).x as isize, (corner + offset).y as isize]
+            }) & 0b11 {
+                0 => point.x + point.y,  // ( 1,  1)
+                1 => -point.x + point.y, // (-1,  1)
+                2 => point.x - point.y,  // ( 1, -1)
+                3 => -point.x - point.y, // (-1, -1)
+                _ => unreachable!(),
+            }
+        }
+    }
+    );
+
+    
+    let g00 = call_gradient!(0., 0.);
+    let g10 = call_gradient!(1., 0.);
+    let g01 = call_gradient!(0., 1.);
+    let g11 = call_gradient!(1., 1.);
+
+    let curve = Vec2 {
+        x: distance.x.map_quintic(),
+        y: distance.y.map_quintic()
+    };
+
+
+    let result = linear(
+        linear(g00, g01, curve.y),
+        linear(g10, g11, curve.y),
+        curve.x,
+    ) * SCALE_FACTOR;
+
+    result.clamp(-1.0, 1.0)
+}
+
+#[derive(Clone, Copy)]
+struct PermutationTable {
+    values: [u8; TABLE_SIZE],
+}
+
+impl Distribution<PermutationTable> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> PermutationTable {
+        let mut perm_table = PermutationTable {
+            values: core::array::from_fn(|x| x as u8),
+        };
+        perm_table.values.shuffle(rng);
+
+        perm_table
+    }
+}
+
+impl PermutationTable {
+    fn new(seed: u32) -> Self {
+        let mut real = [0; 16];
+        real[0] = 1;
+        for i in 1..4 {
+            real[i * 4] = seed as u8;
+            real[i * 4 + 1] = (seed >> 8) as u8;
+            real[i * 4 + 2] = (seed >> 16) as u8;
+            real[i * 4 + 3] = (seed >> 24) as u8;
+        }
+        let mut rng: XorShiftRng = SeedableRng::from_seed(real);
+        rng.gen()
+    }
+}
+
+impl NoiseHasher for PermutationTable {
+    fn hash(&self, to_hash: &[isize]) -> usize {
+        let index = to_hash
+            .iter()
+            .map(|&a| (a & 0xff) as usize)
+            .reduce(|a, b| self.values[a] as usize ^ b)
+            .unwrap();
+        self.values[index] as usize
+    }
+}
